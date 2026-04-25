0b177d0383cf5be8c93e241a4d49ad5872eaba6d
alexey-y-a <lexus2157@mail.ru>
2026-04-25 20:28:19 +0300

    step_002 done

---
+++ src/main/java/ru/alexey/otpsecureservice/OtpSecureServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.alexey.otpsecureservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpSecureServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtpSecureServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/ru/alexey/otpsecureservice/model/OtpStatus.java
@@ -0,0 +1,5 @@
+package ru.alexey.otpsecureservice.model;
+
+public enum OtpStatus {
+    ACTIVE, EXPIRED, USED
+}
